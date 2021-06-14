08ec7c01523c9a11bca0cc0af14b41e3e4815a54
Javier A. Cabrera <cabreraction@gmail.com>
2021-06-14 08:36:55 -0600

    added changes to the exercise of callbacks and emitters

--- Callbacks and Events/The Observer Pattern/exercises.js
+++ Callbacks and Events/The Observer Pattern/exercises.js
@@ -1,15 +1,36 @@
 import { EventEmitter } from 'events';
 
+const validator = () => {
+    const date = Date.now();
+    if (date % 5 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 const ticker = (n, cb) => {
     const emitter = new EventEmitter();
+    
+    let ticks = 1;
+    emitter.emit('tick', ticks);
+
+    if (validator()) {
+        cb('cb error: time stamp is divisible by five', ticks);
+        emitter.emit('error', 'error emit: time stamp divisible by five');
+    }
+
     let counter = 0;
-    let ticks = 0;
     const emitting = () => {
         if (counter < n) {
             setTimeout(() => {
                 counter += 50;
                 ticks++;
-                emitter.emit('tick')
+                emitter.emit('tick', ticks)
+                if (validator()) {
+                    cb('cb error: time stamp is divisible by five', ticks);
+                    emitter.emit('error', 'error emit: time stamp divisible by five');
+                }
                 emitting();
             }, 50)
         } else {
@@ -17,6 +38,12 @@ const ticker = (n, cb) => {
         }
     }
     emitting();
+    return emitter;
 }
 
-ticker(300, (ticks) => console.log(ticks));
+ticker(800, (error, ticks) => {
+    console.error(error);
+    console.log()
+})
+    .on('tick', (ticks) => console.log(`Number of ticks: ${ticks}`))
+    .on('error', error => console.log(error));
