a0150aed1fc5e1840d81ce1c421588eeb34449e2
Javier A. Cabrera <cabreraction@gmail.com>
2021-06-22 06:31:25 -0600

    added callback discipline principles

--- Async Patterns with Callbacks/web-spider/spider.js
+++ Async Patterns with Callbacks/web-spider/spider.js
@@ -33,3 +33,70 @@ export function spider (url, cb) {
         }
     })
 }
+
+export function earlyReturnSpider (url, cb) {
+    const filename = urlToFilename(url);
+    fs.access(filename, err => {
+        if (err && err.code === 'ENOENT') {
+            console.log(`Downloading ${url} into ${filename}`);
+            superagent.get(url).end((err, res) => {
+                if (err) {
+                    return cb(err);
+                }
+                mkdirp(path.dirname(filename), err => {
+                    if(err) {
+                        return cb(err);
+                    } 
+                    fs.writeFile(filename, res.text, err => {
+                        if (err) {
+                            return cb(err);
+                        } 
+                        cb(null, filename, true);
+                    });
+                }); 
+            });
+            return;
+        } 
+        cb(null, filename, false);
+    })
+}
+
+export function modularSpider (url, cb) {
+    const filename = urlToFilename(url);
+    fs.access(filename, err => {
+        if (!err || err.code !== 'ENOENT') {
+            return cb(null, filename, false);
+        }
+        download(url, filename, err => {
+            if (err) {
+                return cb(err);
+            }
+            cb(null, filename, true);
+        });
+    });
+}
+
+function saveFile (filename, contents, cb) {
+    mkdirp(path.dirname(filename), err => {
+        if(err) {
+            return cb(err);
+        } 
+        fs.writeFile(filename, contents, cb);
+    }); 
+}
+
+function download (url, filename, cb) {
+    console.log(`Downloading ${url}`);
+    superagent.get(url).end((err, res) => {
+        if (err) {
+            return cb(err);
+        }
+        saveFile(filename, res.text, err => {
+            if (err) {
+                return cb(err);
+            }
+            console.log(`Downloaded and saved: ${url}`);
+            cb(nul, res.text);
+        }) 
+    });
+}
